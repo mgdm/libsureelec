306fb69680aa4bd51079ead87153bb7ba7bd83b7
Michael Maclean <michael@no-surprises.co.uk>
2011-06-22 19:16:53 +0100

    Disable debugging

--- sureelec_test.c
+++ sureelec_test.c
@@ -47,7 +47,7 @@ int main(int argc, char **argv) {
         return -1;
     }
 
-    ctx = libsureelec_create(argv[1], 1);
+    ctx = libsureelec_create(argv[1], 0);
     
     if (ctx == NULL) {
         printf("Failed to initialize context\n");
